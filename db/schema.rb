ba4443f7b29528d248bbced90c1c6d8108230d2d
Tungg Vu <vutungtn95@gmail.com>
2017-03-24 10:29:23 +0700

    reset password 1.1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170321025524) do
+ActiveRecord::Schema.define(version: 20170324021027) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -23,6 +23,9 @@ ActiveRecord::Schema.define(version: 20170321025524) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.string   "reset_sent_at"
+    t.string   "datetime"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
